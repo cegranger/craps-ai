9ea01236f0ee6cd6573b174a4bb0d36713003736
me in the past <grac2310@usherbrooke.ca>
2024-12-20 15:28:13 -0500

    inital commit

---
+++ data/states/bingo/events.py
@@ -0,0 +1,8 @@
+"""Events for the bingo game"""
+
+E_PLAYER_PICKED = 'player-picked'
+E_PLAYER_UNPICKED = 'player-unpicked'
+E_NUM_CARDS_CHANGED = 'number-of-cards-changed'
+E_SPEND_MONEY = 'spend-money'
+E_CARD_COMPLETE = 'card-complete'
+E_BONUS_REACHED = 'bonus-reached'
